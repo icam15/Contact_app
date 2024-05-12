9f35d486f6d5dbbaee41397be84e6de74d469900
icam15 <159884438+icam15@users.noreply.github.com>
2024-05-12 07:46:20 +0700

    Feat/auth (#3)
    * wip : add user register
    
    * feat(auth): add login and register
    
    * feat(auth): add login service google-oauth 2.0
    
    * add: jest unit test
    
    * feat(auth): add new access token api
    
    ---------
    
    Co-authored-by: syam achmad ali <biasnkiza.gmail.com>

--- src/auth/auth-repository.js
+++ src/auth/auth-repository.js
@@ -2,59 +2,70 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const checkUserExist = async (email) => {
-    const check = await prisma.user.findFirst({
-        where:{
-            email,
-        },
-        select: {
-            id: true,
-            email: true,
-            password: true
-        }
-    })
+  const check = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+    },
+  });
 
-    return check
-}
+  return check;
+};
 
 const createNewUser = async (newUserData) => {
-    const create = await prisma.user.create({
-        data:{
-            email: newUserData.email,
-            password: newUserData.password,
-            username: newUserData.username
-        }
-    })
+  const create = await prisma.user.create({
+    data: {
+      email: newUserData.email,
+      password: newUserData.password,
+      username: newUserData.username,
+    },
+  });
 
-    return create
-}
+  return create;
+};
 
 const addRefreshTokenUser = async (id, refreshToken) => {
-    const addToken = await prisma.user.update({
-        where:{
-            id
-        },
-        data:{
-            refresh_token: refreshToken
-        }
-    });
+  const addToken = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      refresh_token: refreshToken,
+    },
+  });
 
-    return addToken
-}
+  return addToken;
+};
 
-const createUserGoogle = async (userGoogleData) =>{
-    const create = await prisma.user.create({
-        data: {
-            email: userGoogleData.email,
-            username: userGoogleData.name
-        }
-    })
-    
-    return create
-}
+const createUserGoogle = async (userGoogleData) => {
+  const create = await prisma.user.create({
+    data: {
+      email: userGoogleData.email,
+      username: userGoogleData.name,
+    },
+  });
+
+  return create;
+};
+
+const findUserToken = async (token) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      refresh_token: token,
+    },
+  });
+  return user;
+};
 
 export {
-    checkUserExist,
-    createNewUser,
-    addRefreshTokenUser,
-    createUserGoogle
-}
+  checkUserExist,
+  createNewUser,
+  addRefreshTokenUser,
+  createUserGoogle,
+  findUserToken,
+};
+
