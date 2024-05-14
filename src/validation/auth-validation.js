0071978feb2080f8bc7c1386ba4d2b5071023730
syam achmad ali <biasnkiza.gmail.com>
2024-05-14 15:40:38 +0700

    validation: add error handling and auth validation

--- src/validation/auth-validation.js
+++ src/validation/auth-validation.js
@@ -1,8 +1,14 @@
-import Joi from "joi"
+import Joi from "joi";
 
-const userRegisterValidaion = Joi.object({
-    email: Joi.string().max(200).required(),
-    password: Joi.string().max(10).required(),
-    username: Joi.string().max(20).required(),
-    phone: Joi.number().max(12).required()
+const userRegisterValidation = Joi.object({
+  email: Joi.string().max(200).email().required(),
+  password: Joi.string().max(10).required(),
+  username: Joi.string().max(20).required(),
 });
+
+const userLoginValidation = Joi.object({
+  email: Joi.string().max(200).email().required(),
+  password: Joi.string().max(10).required(),
+});
+
+export { userRegisterValidation, userLoginValidation };
